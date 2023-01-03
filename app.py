126cdb51f074e9424f6718b7b1375e70308a25db
Roman Mendaliev <reacheight8@gmail.com>
2023-01-03 19:30:45 +0300

    Add route to get watching anime and some refactoring

--- app.py
+++ app.py
@@ -11,4 +11,16 @@ CORS(app, origins=['https://twitch.tv', 'https://localhost:8080'])
 @app.route('/<profile>/anime/top')
 def get_top_anime(profile):
     limit = request.args.get('limit', config.DEFAULT_ANIME_LIMIT)
-    return jsonify(mal_client.get_top_rated_anime(profile, limit))
+    return jsonify(mal_client.top_rated_anime(profile, limit))
+
+
+@app.route('/<profile>/anime/recently-finished')
+def get_recently_finished_anime(profile):
+    limit = request.args.get('limit', config.DEFAULT_ANIME_LIMIT)
+    return jsonify(mal_client.recently_finished_anime(profile, limit))
+
+
+@app.route('/<profile>/anime/watching')
+def get_watching_anime(profile):
+    limit = request.args.get('limit', config.DEFAULT_ANIME_LIMIT)
+    return jsonify(mal_client.watching_anime(profile, limit))
--- mal_client.py
+++ mal_client.py
@@ -1,27 +1,51 @@
 import os
-
 import requests
 
 import config
 
 
-def get_top_rated_anime(profile, limit=config.DEFAULT_ANIME_LIMIT):
-    url = f'{config.MAL_HOST}/v2/users/{profile}/animelist?status=completed&sort=list_score&limit={limit}&fields=list_status'
+def top_rated_anime(profile, limit=config.DEFAULT_ANIME_LIMIT):
+    url = build_url(profile, 'completed', 'list_score', limit)
+    return make_request(url)
+
+
+def recently_finished_anime(profile, limit=config.DEFAULT_ANIME_LIMIT):
+    url = build_url(profile, 'completed', 'anime_start_date', limit)
+    return make_request(url)
+
+
+def watching_anime(profile, limit=config.DEFAULT_ANIME_LIMIT):
+    url = build_url(profile, 'watching', 'anime_start_date', limit)
+    return make_request(url)
+
+
+def make_request(url):
     client_id = os.environ.get('MAL_CLIENT_ID')
     response = requests.get(url, headers={'X-MAL-CLIENT-ID': client_id})
     response.raise_for_status()
 
-    anime_data = response.json()['data']
+    return map_response_to_anime_list(response.json())
+
+
+def build_url(profile, status, sort, limit):
+    return f'{config.MAL_HOST}/v2/users/{profile}/animelist?status={status}&sort={sort}&limit={limit}&fields=list_status'
+
+
+def map_response_to_anime_list(json):
+    data = json['data']
     anime_list = [
-        build_user_anime(data['node']['id'], data['node']['title'], data['node']['main_picture']['medium'], data['list_status']['score'])
-        for data in anime_data]
+        build_user_anime(entry['node']['id'], entry['node']['title'], entry['node']['main_picture']['medium'],
+                         entry['list_status']['score'], entry['list_status']['num_episodes_watched'])
+        for entry in data]
+
     return anime_list
 
 
-def build_user_anime(anime_id, title, picture, score):
+def build_user_anime(anime_id, title, picture, score, episodes_watched):
     return {
         'id': anime_id,
         'title': title,
         'picture': picture,
-        'score': score
+        'score': score,
+        'episodes_watched': episodes_watched
     }
