abde2fd6a5515a65578b1260043f9fed69bdaba1
Roman Mendaliev <reacheight8@gmail.com>
2023-01-03 15:20:34 +0300

    Allow localhost for local testing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import mal_client
 
 app = Flask(__name__)
-CORS(app, origins=['https://twitch.tv'])
+CORS(app, origins=['https://twitch.tv', 'https://localhost:8080'])
 
 
 @app.route('/<profile>/anime/top')
