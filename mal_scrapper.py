a5f15b9f18744b1b3883ea0dcc8acff8d7bd5f55
Roman Mendaliev <reacheight8@gmail.com>
2026-03-29 22:22:56 +0300

    fix joined date scrapping

--- mal_scrapper.py
+++ mal_scrapper.py
@@ -10,6 +10,11 @@ def get_user_info(username):
 
     user_profile_div = soup.find('div', class_='user-profile')
     img_element = user_profile_div.find('img')
-    joined_element = user_profile_div.find_all('li', class_='clearfix')[1].contents[1].string
+    joined_li = next(
+        (li for li in user_profile_div.find_all('li', class_='clearfix')
+        if li.find('span', class_='user-status-title') and li.find('span', class_='user-status-title').string == 'Joined'),
+        None
+    )
+    joined_element = joined_li.find('span', class_='user-status-data').string if joined_li else None
 
     return { 'pic': img_element['data-src'] if img_element else None, 'joined': joined_element }
